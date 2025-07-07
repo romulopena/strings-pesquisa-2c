9682c0da615823fa6d816d0fd9da96c41e8c7da0
PROF ROMULO <rjlpena@gmail.com>
2025-07-07 11:33:17 +0000

    aula 3

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-function mostraPalavrasChave(){
-    alert('Fui Clicado!')
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavras = texto.split(" ");
+    
+    campoResultado.textContent = palavras.join(", ");
+    
 }
+
